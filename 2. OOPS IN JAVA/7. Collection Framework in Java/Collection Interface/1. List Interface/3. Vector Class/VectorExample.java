3a95ed1eb223a778ba5f1664cecdc8567e81139c
Vishgun@0287. <vishalgunjal0287@gmail.com>
2025-04-10 18:57:26 +0530

    Add 1. List Interface - Vector Class - Vector Example

--- 2. OOPS IN JAVA/7. Collection Framework in Java/Collection Interface/1. List Interface/3. Vector Class/VectorExample.java
+++ 2. OOPS IN JAVA/7. Collection Framework in Java/Collection Interface/1. List Interface/3. Vector Class/VectorExample.java
@@ -7,7 +7,7 @@ public class VectorExample {
         // Create Integer type vector
         List<Integer> vector1 = new Vector<>();
 
-        //? Common Methods of Collection for All Sub-Interfaces
+        // ? Common Methods of Collection for All Sub-Interfaces
         // Method 1: add() - inserts the specified element to the collection
         vector1.add(10);
         vector1.add(20);
@@ -25,26 +25,29 @@ public class VectorExample {
         System.out.println("Output 3: " + vector1);
         // Output: [20, 30]
 
-        // Method 4: iterator() - returns an iterator to access elements of the collection
+        // Method 4: iterator() - returns an iterator to access elements of the
+        // collection
         Iterator<Integer> iterate = vector1.iterator();
-        while(iterate.hasNext()) {
+        while (iterate.hasNext()) {
             System.out.print(iterate.next());
             System.out.print(", ");
         }
         System.out.println();
         // Output: 20, 30,
 
-        // Method 5: addAll() - adds all the elements of a specified collection to the collection
+        // Method 5: addAll() - adds all the elements of a specified collection to the
+        // collection
         Vector<Integer> vector2 = new Vector<>();
         vector2.add(100);
         System.out.println("Output 4: " + vector2);
         // Output: [100]
-        
+
         vector1.addAll(vector2);
         System.out.println("Output 5: " + vector1);
         // Output: [20, 30, 100]
 
-        // Method 6: removeAll() - removes all the elements of the specified collection from the collection
+        // Method 6: removeAll() - removes all the elements of the specified collection
+        // from the collection
         vector1.removeAll(vector2);
         System.out.println("Output 6: " + vector1);
         // Output: [20, 30]
@@ -54,7 +57,7 @@ public class VectorExample {
         System.out.println("Output 7: " + vector1);
         // Output: []
 
-        //? Methods of Vector Class
+        // ? Methods of Vector Class
         // Method add(index, element) - adds an element to the specified position
         vector1.add(0, 40);
         vector1.add(1, 50);
